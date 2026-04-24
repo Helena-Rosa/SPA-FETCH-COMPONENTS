3579543b739e66ad7866aceb07e97d7c40297ee1
Helena Rosa <helena.rosa@aluno.senai.br>
2026-04-24 08:59:37 -0300

    testando

---
+++ test/test.js
@@ -0,0 +1,33 @@
+/* teste com os exemplos de w3 school*/
+
+try{
+
+}catch (erro){
+    
+    let text = `
+    <strong>Nome do erro:</strong> ${erro.name} <br />
+    <strong>Mensagem:</strong> ${erro.message} <br />
+    <strong>Stack:</strong> <pre>${erro.stack} <br />
+    `;
+
+    document.body.innerHTML = text 
+
+};
+
+
+
+/*lançar um excessã (erro personalizado)*/
+
+/*criar uma função geradora de erro*/
+
+/* testando a função: carregarComponente 
+01- Criar o componente que erá injetado 
+02- Criar o elementoque irá receber o componente (no inex index.html) (<div id="app")
+03- capturar o elemento que irpa receber o componente ( no index.html)
+04- Utilizar a função para carregar o componente*/
+
+
+/* criar uma promessa e consumir usando then e .catch*/
+
+/* cosumir usando async/awaint e try.catch*/
+
