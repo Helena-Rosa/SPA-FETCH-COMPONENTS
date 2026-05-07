f0abda97bbdaab9fedd985d2ed83c7ab0390a477
Helena Rosa <helena.rosa@aluno.senai.br>
2026-05-07 09:05:42 -0300

    fazendo os botao do cep

--- test/cap/script-cep.js
+++ test/cap/script-cep.js
@@ -1,6 +1,10 @@
 const url = 'https://viacep.com.br/ws/01001000/json/'
 const consulta = fetch(url);
+const btn_1 = document.querySelector ('.btn_1')
+const btn_2 = document.querySelector ('.btn_2')
+
 consulta 
+
 .then((resposta) => {
 
         if(!response.ok){
@@ -21,3 +25,48 @@ consulta
         console.warn(erro.message)
 
     });
+
+const input = document.querySelector('.input_cep');
+
+const url = `https://viacep.com.br/ws/${input}/json/`;
+
+
+async function buscarDados() {
+    try 
+        console.log("Buscando...");
+        
+        
+        const resposta = await fetch(url);
+        
+        if (!resposta.ok) {
+            throw new Error("Erro de conexão com o servidor.");
+        }
+
+        
+        const dados = await resposta.json();
+
+        if (dados.erro) {
+            throw new Error("CEP não encontrado.");
+        }
+    }
+
+
+
+
+
+
+
+
+
+    const url = 'https://viacep.com.br/ws/01001000/json/';
+    const consulta = fetch(url);
+
+    async function cep(){
+        let mensagem = await consulta;
+        console.log (mensagem)
+        let data = await mensagem.json();
+        console.log(data);
+
+    };
+
+    cep();
